806969ff6d8da7fb6012f750b9338afab5fff2f7
Samuel Yuan <samuelyuan@users.noreply.github.com>
2016-02-05 00:57:37 +0000

    Homepage set up

---
+++ server.js
@@ -0,0 +1,84 @@
+var request = require("request"),
+	cheerio = require("cheerio"),
+    express = require("express");
+var app     = express();
+var path = require ('path');
+    
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/',function(req,res){
+     res.render('index');
+});
+
+app.get('/results',function(req, res){
+    var dataString = "";
+    
+    // download that page
+    request(req.query.url, function (error, response, body) {
+        if (error) {
+            console.log("Couldn’t get page because of error: " + error);
+            return;
+        }
+
+        // load the page into cheerio
+        var $page = cheerio.load(body),
+            text = $page("body").text();
+
+        // split data into sentences using a period as a separator
+        var sentenceData = [];
+        
+        var lineNumberProfile = 0;
+        var startDisplayLine = 0;
+        var endDisplayLine = 0;
+        
+        //First find the start and end markers for displaying content
+        text.split(".").forEach(function (sentence) {
+            sentence = sentence.replace(/\s+/g, " ")
+                                .replace(/[^a-zA-Z ]/g, "");
+            
+            lineNumberProfile++;
+            
+            if (sentence.toLowerCase().indexOf("profile") > -1)
+            {
+                //The actual forum post data starts from here (line #), not from line #0. 
+                if (startDisplayLine == 0)
+                {
+                    startDisplayLine = lineNumberProfile;
+                }
+            }
+            
+            //Usually placed at the end of a thread
+            if (sentence.toLowerCase().indexOf("login") > -1 || sentence.toLowerCase().indexOf("log in") > -1 )
+            {
+                endDisplayLine = lineNumberProfile;
+            }
+        });
+        
+        //Display the data
+        lineNumberProfile = 0;
+        text.split(".").forEach(function (sentence) {
+             // throw away extra whitespace and non-alphanumeric characters
+            sentence = sentence.replace(/\s+/g, " ")
+                   .replace(/[^a-zA-Z ]/g, "");
+        
+            lineNumberProfile++;
+            
+             //Ignore anything that isn't an actual post
+            if (lineNumberProfile >= startDisplayLine && lineNumberProfile <= endDisplayLine)
+            {
+                //Add sentence to overall data
+                sentenceData.push(sentence);
+            }
+        });
+        
+        res.render('results', {
+            sentenceData: sentenceData
+        });
+    });
+    
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
